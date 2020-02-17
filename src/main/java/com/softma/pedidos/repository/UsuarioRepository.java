41e7342160ef9c644f9d83f50f1fc59a79793b30
Pedro Thiago <pedrotrm@outlook.com.br>
2020-02-17 15:40:58 -0300

    configurando JPA e ambiente de teste

---
+++ src/main/java/com/softma/pedidos/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.softma.pedidos.repository;
+
+import com.softma.pedidos.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long > {
+
+
+
+
+}
